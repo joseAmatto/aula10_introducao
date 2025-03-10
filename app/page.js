f90d8cad19d1082d6aca1b8b59099303a5ff0b03
joseAmatto <amattonetto@gmail.com>
2025-03-10 14:27:42 -0300

    versão base do codigo

---
+++ app/page.js
@@ -0,0 +1,48 @@
+'use client'
+
+import { useState } from "react";
+import "./home.css"
+
+
+export default function Home() {
+
+   let [carrinho, setCarrinho] = useState(0)
+
+
+   
+
+
+
+
+
+  return (
+    
+        <div >
+            
+        
+       
+
+           <h1> Conradito Market </h1>    
+           <p> O melhor supermercado do bairro! </p>   
+           <hr/>
+           <p className="carrinho"> itens no carrinho: {carrinho} </p> 
+           <hr/>
+
+
+           <h2> Produtos </h2>
+           <div className="produto">
+
+               <img src="https://placehold.co/150"/>
+                <p> Cenoura </p>
+                <p> R$: 4,50 (kg)</p>
+                <button onClick={()=> setCarrinho()  }> Comprar </button>
+            </div>
+        </div>
+
+
+    );
+}
+
+
+
+
