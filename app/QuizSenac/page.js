9eef1f140b27d4c88e070a1095281767945f971f
joseAmatto <amattonetto@gmail.com>
2025-03-13 14:46:10 -0300

    projeto

--- app/QuizSenac/page.js
+++ app/QuizSenac/page.js
@@ -3,9 +3,24 @@ import { useState } from "react";
 import "./projeto.css";
 
 function QuizSenac() {
-  const [p1, setP1] = useState(0);
+
+  const [p1, setP1] = useState(0)
+  const[p2, setP2] = useState(0)
+  const[p3, setP3] = useState(0)
+  const[p4, setP4] = useState(0)
+  const[p5, setP5] = useState(0)
+
+
   const [selecionado, setSelecionado] = useState(["", "", "", ""]);
+
   const [ShowP1, setShowP1] = useState(true);
+  const[ShowP2, setShowP2] = useState(false)
+  const[ShowP3, setShowP3] = useState(false)
+  const[ShowP4, setShowP4] = useState(false)
+  const[ShowP5, setShowP5] = useState(false)
+
+  function validaNome(){
+
 
   return (
     <div>
@@ -19,7 +34,7 @@ function QuizSenac() {
 
           <br />
           <div className="container">
-            
+
                     <div className="opcao">
                         <button name="p1" className={selecionado[0]} onClick={()=>{ setP1(1); setSelecionado(["selecionado","","",""])}}> Pikachu </button>
                     </div>
